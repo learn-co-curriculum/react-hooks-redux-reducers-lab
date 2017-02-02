443cc1039fde49077f4f89017ad6a0abc17d5152
Jeff Katz <jeffreyerickatz@gmail.com>
2017-02-02 16:31:02 -0500

    update redux reducer for lab

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,8 +1,2 @@
 export function managePresents(state, action){
-  switch (action.type) {
-    case "INCREASE":
-      return {numberOfPresents: state.numberOfPresents + 1}
-    default:
-      return state;
-  }
 }
