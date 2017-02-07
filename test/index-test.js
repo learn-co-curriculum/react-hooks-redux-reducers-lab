a0e892917bb33825807049d2d4023f839cf6d622
Luke Ghenco <lukeghenco@gmail.com>
2017-02-07 04:47:26 -0500

    remove payload context from tests.

--- test/index-test.js
+++ test/index-test.js
@@ -27,13 +27,13 @@ describe('manageFriends', function() {
     expect(manageFriends(state, {type: 'Random Action Type'})).toEqual(state);
   })
 
-  it("adds the friend when type is 'ADD_FRIEND' and the action has a payload property with the friend", function(){
-    expect(manageFriends(state, {type: "ADD_FRIEND", payload: {name: 'Joe', hometown: 'Boston', id: 101}})).toEqual({numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Joe', hometown: 'Boston', id: 101}]});
+  it("adds the friend when type is 'ADD_FRIEND' and the action has a friend property with a name, hometown and id", function(){
+    expect(manageFriends(state, {type: "ADD_FRIEND", friend: {name: 'Joe', hometown: 'Boston', id: 101}})).toEqual({numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Joe', hometown: 'Boston', id: 101}]});
   })
 
-  it("removes the friend when action type is 'REMOVE_FRIEND' and the action has a payload property of the friend's id to be removed", function(){
+  it("removes the friend when action type is 'REMOVE_FRIEND' and the action has a property of friendId to be removed", function(){
     let state = {numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Joe', hometown: 'Boston', id: 101}, {name: 'Steven', hometown: 'Philadephia', id: 102}]}
-    expect(manageFriends(state, {type: "REMOVE_FRIEND", payload: 101})).toEqual({numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Steven', hometown: 'Philadephia', id: 102}]});
+    expect(manageFriends(state, {type: "REMOVE_FRIEND", friendId: 101})).toEqual({numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Steven', hometown: 'Philadephia', id: 102}]});
   })
 
   it("adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object", function(){
