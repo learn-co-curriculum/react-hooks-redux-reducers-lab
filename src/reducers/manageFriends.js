6e1c1e07d78cd33deaf3efdee259da14fe904f68
Luke Ghenco <lukeghenco@gmail.com>
2017-02-12 06:01:30 -0500

    update manageFriends.js solution to match tests

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,33 +1,27 @@
-export function manageFriends(state, action){
-  switch (action.type) {
+export function manageFriends(state = {
+  friends: [],
+}, action) {
+  switch(action.type) {
+
     case "ADD_FRIEND":
-      // ES5 way:
-      // return Object.assign(
-      //   {},
-      //   state,
-      //   {friends: state.friends.concat([action.payload])}
-      //  );
-      return {
-        ...state,
-        friends: [...state.friends, action.payload]
-      }
+      return Object.assign({}, state, {
+        friends: [
+          ...state.friends,
+          action.friend
+        ]
+      });
+
     case "REMOVE_FRIEND":
-      let indexToRemove = state.friends.findIndex( friend =>
-        friend.id === action.payload );
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return Object.assign({}, state, {
+        friends: [
+          ...state.friends.slice(0, removalIndex),
+          ...state.friends.slice(removalIndex + 1)
+        ]
+      });
 
-      // ES5 way
-      // let newFriends = state.friends.slice(0, indexToRemove)
-      //   .concat(state.friends.slice(indexToRemove + 1));
-      let newFriends = [
-        ...state.friends.slice(0, indexToRemove),
-        ...state.friends.slice(indexToRemove + 1)
-      ]
-
-      // ES5 way
-      // return Object.assign({}, state, {friends: newFriends});
-      return {...state, friends: newFriends};
     default:
       return state;
 
   }
-}
+};
