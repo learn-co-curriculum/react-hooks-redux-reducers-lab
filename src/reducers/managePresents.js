db5c0b31d04e6f3d758596b6ea6e10573775334c
Luke Ghenco <lukeghenco@gmail.com>
2017-02-12 06:04:56 -0500

    Update managePresents to pass updated test

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,13 @@
-export function managePresents(state, action){
-  switch (action.type) {
+export function managePresents(state = {
+  numberOfPresents: 0,
+}, action) {
+  switch(action.type) {
+
     case "INCREASE":
-      return {numberOfPresents: state.numberOfPresents + 1}
+      return Object.assign({}, state, {
+        numberOfPresents: state.numberOfPresents + 1
+      });
+
     default:
       return state;
   }
