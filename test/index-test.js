7886524f1f8025e15f3fbf208fae18a05a669bf9
Luke Ghenco <lukeghenco@gmail.com>
2017-02-12 05:53:28 -0500

    update tests

--- test/index-test.js
+++ test/index-test.js
@@ -1,44 +1,157 @@
 import { managePresents } from '../src/reducers/managePresents';
 import { manageFriends } from '../src/reducers/manageFriends';
-import sinon        from 'sinon';
+import sinon from 'sinon';
 
-describe('managePresents', function() {
+describe('managePresents', () => {
+  let state = {
+    numberOfPresents: 0,
+    friends: [],
+  };
 
-  let state = {numberOfPresents: 0, friends: []}
+  it("returns the existing state if the action's type doesn't match a type in the reducer", () => {
+    expect(managePresents(state, { type: 'Random Action Type' })).toEqual(state);
+  });
 
-  it("returns the existing state if the action's type doesn't match a type in the reducer", function() {
-    expect(managePresents(state, {type: 'Random Action Type'})).toEqual(state);
-  })
+  it("increases the number of presents if there the action's type is 'INCREASE'", () =>{
+    expect(managePresents(state, { type: "INCREASE" })).toEqual({
+      numberOfPresents: 1,
+      friends: []
+    });
+  });
 
-  it("increases the number of presents if there the action's type is 'INCREASE'", function(){
-    expect(managePresents(state, {type: "INCREASE"})).toEqual({numberOfPresents: 1});
-  })
-
-  it("adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object", function(){
-    managePresents(state, {type: "INCREASE"})
-    expect(state).toEqual({numberOfPresents: 0, friends: []})
-  })
-})
-
-describe('manageFriends', function() {
-  let state = {numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}]}
-
-  it("returns the existing state if the action's type doesn't match a type in the reducer", function() {
-    expect(manageFriends(state, {type: 'Random Action Type'})).toEqual(state);
-  })
-
-  it("adds the friend when type is 'ADD_FRIEND' and the action has a payload property with the friend", function(){
-    expect(manageFriends(state, {type: "ADD_FRIEND", payload: {name: 'Joe', hometown: 'Boston', id: 101}})).toEqual({numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Joe', hometown: 'Boston', id: 101}]});
-  })
-
-  it("removes the friend when action type is 'REMOVE_FRIEND' and the action has a payload property of the friend's id to be removed", function(){
-    let state = {numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Joe', hometown: 'Boston', id: 101}, {name: 'Steven', hometown: 'Philadephia', id: 102}]}
-    expect(manageFriends(state, {type: "REMOVE_FRIEND", payload: 101})).toEqual({numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Steven', hometown: 'Philadephia', id: 102}]});
-  })
-
-  it("adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object", function(){
-    let state = {numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Joe', hometown: 'Boston', id: 101}, {name: 'Steven', hometown: 'Philadephia', id: 102}]}
-    manageFriends(state, {type: "REMOVE_FRIEND", payload: 'Joe'})
-    expect(state).toEqual({numberOfPresents: 0, friends: [{name: 'Avi', hometown: 'NYC', id: 100}, {name: 'Joe', hometown: 'Boston', id: 101}, {name: 'Steven', hometown: 'Philadephia', id: 102}]})
+  it("adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object", () =>{
+    managePresents(state, { type: "INCREASE" });
+    expect(state).toEqual({
+      numberOfPresents: 0,
+      friends: [],
+    });
   })
 })
+
+describe('manageFriends', () => {
+  let state = {
+    numberOfPresents: 0,
+    friends: [
+      {
+        name: 'Avi',
+        hometown: 'NYC',
+        id: 100
+      }
+    ],
+  };
+
+  it("returns the existing state if the action's type doesn't match a type in the reducer", () => {
+    expect(manageFriends(state, { type: 'Random Action Type' })).toEqual(state);
+  });
+
+  it("adds the friend when type is 'ADD_FRIEND' and the action has a friend property with a name, hometown and id", () =>{
+    expect(manageFriends(state, {
+      type: "ADD_FRIEND",
+      friend: {
+        name: 'Joe',
+        hometown: 'Boston',
+        id: 101
+      }
+    })).toEqual({
+      numberOfPresents: 0,
+      friends: [
+        {
+          name: 'Avi',
+          hometown: 'NYC',
+          id: 100
+        },
+        {
+          name: 'Joe',
+          hometown: 'Boston',
+          id: 101
+        }
+      ]
+    });
+  });
+
+  it("removes the friend when action type is 'REMOVE_FRIEND' and the action has a property of the friends id to be removed", () =>{
+    let state = {
+      numberOfPresents: 0,
+      friends: [
+        {
+          name: 'Avi',
+          hometown: 'NYC',
+          id: 100
+        },
+        {
+          name: 'Joe',
+          hometown: 'Boston',
+          id: 101
+        },
+        {
+          name: 'Steven',
+          hometown: 'Philadephia',
+          id: 102
+        }
+      ]
+    };
+
+    expect(manageFriends(state, {
+      type: "REMOVE_FRIEND",
+      id: 101
+    })).toEqual({
+      numberOfPresents: 0,
+      friends: [
+        {
+          name: 'Avi',
+          hometown: 'NYC',
+          id: 100
+        },
+        {
+          name: 'Steven',
+          hometown: 'Philadephia',
+          id: 102
+        }
+      ]
+    });
+  });
+
+  it("adheres to the rules of being a pure function, by not changing the original state, and instead returning a new object", () =>{
+    let state = {
+      numberOfPresents: 0,
+      friends: [
+        {
+          name: 'Avi',
+          hometown: 'NYC',
+          id: 100
+        },
+        {
+          name: 'Joe',
+          hometown: 'Boston',
+          id: 101
+        },
+        {
+          name: 'Steven',
+          hometown: 'Philadephia',
+          id: 102
+        }
+      ]
+    };
+    manageFriends(state, { type: "REMOVE_FRIEND", id: 101 });
+    expect(state).toEqual({
+      numberOfPresents: 0,
+      friends: [
+        {
+          name: 'Avi',
+          hometown: 'NYC',
+          id: 100
+        },
+        {
+          name: 'Joe',
+          hometown: 'Boston',
+          id: 101
+        },
+        {
+          name: 'Steven',
+          hometown: 'Philadephia',
+          id: 102
+        }
+      ]
+    });
+  });
+});
