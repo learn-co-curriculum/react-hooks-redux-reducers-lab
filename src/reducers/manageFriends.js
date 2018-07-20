c8be285ac2ca9bf422e4fcf8a20ce492c388cad9
maxwell <github email address>
2018-07-20 13:26:34 -0400

    reseting code to master

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,27 +1,2 @@
-export function manageFriends(state = {
-  friends: [],
-}, action) {
-  switch(action.type) {
-
-    case "ADD_FRIEND":
-      return Object.assign({}, state, {
-        friends: [
-          ...state.friends,
-          action.friend
-        ]
-      });
-
-    case "REMOVE_FRIEND":
-      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
-      return Object.assign({}, state, {
-        friends: [
-          ...state.friends.slice(0, removalIndex),
-          ...state.friends.slice(removalIndex + 1)
-        ]
-      });
-
-    default:
-      return state;
-
-  }
-};
+export function manageFriends(state, action){
+}
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,14 +1,2 @@
-export function managePresents(state = {
-  numberOfPresents: 0,
-}, action) {
-  switch(action.type) {
-
-    case "INCREASE":
-      return Object.assign({}, state, {
-        numberOfPresents: state.numberOfPresents + 1
-      });
-
-    default:
-      return state;
-  }
-};
+export function managePresents(state, action){
+}
