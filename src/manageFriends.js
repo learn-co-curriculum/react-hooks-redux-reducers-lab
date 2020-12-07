61dd022f952bb75a4b4c4731f6109b129a9ca811
ihollander <ianholla@gmail.com>
2020-12-07 18:50:01 -0500

    Added solution

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,14 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [...state.friends, action.payload],
+      };
+    case "friends/remove":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
+    default:
+      return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,8 @@
-const initialState = { numberOfPresents: 0 };
-
-export function managePresents(state = initialState, action) {
-  // your code here
+export function managePresents(state, action) {
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
