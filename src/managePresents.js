5ed6e47efdf3e8bc26b0c4daf5477eacba6011fc
danieljuma5 <jumadaniel275@gmail.com>
2023-07-15 10:45:50 +0300

    fixed issue with the friend/remove action test

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,15 @@
 const initialState = { numberOfPresents: 0 };
 
+let action = {
+    type: "presents/increase",
+  };
 export function managePresents(state = initialState, action) {
-  // your code here
+
+   switch(action.type) {
+    case "presents/increase":
+      return {numberOfPresents:  initialState.numberOfPresents + 1}
+      default:
+        return state
+   }
+   
 }
